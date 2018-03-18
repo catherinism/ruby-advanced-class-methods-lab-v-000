3ffc5872622443fb422736f927e78f4a18e82825
Catherine Salazar <catcreates13@gmail.com>
2018-03-18 20:39:24 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,8 +43,8 @@ end
 def self.new_from_filename(file_name)
   song = self.new
   file = file_name.split(" - ")
-  artist_name = file[0]
-  name = file[1].chomp(".mp3")
+  song.artist_name = file[0]
+  song.name = file[1].chomp(".mp3")
   song
 
 
